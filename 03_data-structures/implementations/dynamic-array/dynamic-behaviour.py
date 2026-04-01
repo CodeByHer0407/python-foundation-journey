2997e941db07486b26b4ffd9ee3542dd3213f5fc
Saloni Azad <saloni.azad4799@gmail.com>
2026-04-02 00:11:21 +0530

    Add more concepts on custom list

--- 03_data-structures/implementations/dynamic-array/dynamic-behaviour.py
+++ 03_data-structures/implementations/dynamic-array/dynamic-behaviour.py
@@ -7,3 +7,15 @@ print("Initial size:", sys.getsizeof(l1))
 for i in range(20):
     l1.append(i)
     print(f"After inserting {i}: {sys.getsizeof(l1)} bytes")
+
+
+c = "python"
+print(c[:-1])
+
+l1 = [12, 31, 14, 51]
+print(l1.pop())
+print(l1)
+
+## Indexing behavior
+
+print(l1[1])
