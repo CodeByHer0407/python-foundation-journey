2e4a7485b5d950a132c5a5789d4d87b1610be74a
Saloni Azad <saloni.azad4799@gmail.com>
2026-04-27 23:19:00 +0530

    Solve question on finding the first and last element in the array

--- 11_recursion/array-problems/03_first_index_of_element.py
+++ 11_recursion/array-problems/03_first_index_of_element.py
@@ -1,5 +1,23 @@
-def first_index(arr1):
-    pass
+def first_index(arr1, val):
+    if len(arr1) == 0:
+        return -1
+    if val == arr1[0]:
+        return 0
+    result = first_index(arr1[1:], val)
+    if result == -1 :
+        return -1
+    return 1 + result
+
+def last_index(arr1, val):
+    if len(arr1) == 0 :
+        return -1
+    result = last_index(arr1[1:], val)
+    if result != -1 :
+        return 1 + result
+    if val == arr1[0]:
+        return 0
+    return -1
+
 
 def create_array(n):
     arr = []
@@ -8,12 +26,16 @@ def create_array(n):
         arr.append(val)
     return arr
 
+
 def main():
     n = int(input("Enter the size of the array: "))
     my_arr1 = create_array(n)
 
     print(f"The array is: {my_arr1}")
 
+    element = int(input("Enter the element whose first index you want to search: "))
+    print(f"The first index of {element}: {first_index(my_arr1, element)}")
 
+    print(f"The last index of {element}: {last_index(my_arr1, element)}")
 if __name__ == "__main__":
     main()
